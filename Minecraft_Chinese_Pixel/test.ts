d15227b02754cd815c8b47655e7a6afabfc1913c
JackChung-Taiwan <58715515+JackChung-Taiwan@users.noreply.github.com>
2026-07-14 00:27:59 +0800

    Simplify Chinese pixel examples to selected features

--- Minecraft_Chinese_Pixel/test.ts
+++ Minecraft_Chinese_Pixel/test.ts
@@ -9,56 +9,26 @@ player.onChat("中文", function () {
     )
 })
 
-player.onChat("看板", function () {
-    chinesePixel.drawBoard(
-        "歡迎來到翊華教育",
-        pos(0, 1, 5),
-        EAST,
-        ChinesePixelColor.Black,
-        ChinesePixelColor.White,
-        ChinesePixelFont.Bold,
-        1,
-        1,
-        2
-    )
-})
-
-player.onChat("發光", function () {
-    chinesePixel.drawGlowText(
-        "任務開始",
-        pos(0, 1, 5),
-        EAST,
-        ChinesePixelFont.Bold,
-        1,
-        1
-    )
-})
-
 player.onChat("直排", function () {
     chinesePixel.drawVerticalText(
         "翊華教育",
         pos(0, 1, 5),
         EAST,
         ChinesePixelColor.Black,
-        ChinesePixelMaterial.Concrete,
         ChinesePixelFont.Bold,
         1,
         1
     )
 })
 
-player.onChat("彩虹", function () {
-    chinesePixel.drawRainbowText(
-        "彩虹麥塊",
+player.onChat("字型", function () {
+    chinesePixel.drawTextWithFont(
+        "程式設計",
         pos(0, 1, 5),
         EAST,
-        ChinesePixelMaterial.Concrete,
-        ChinesePixelFont.Bold,
+        ChinesePixelColor.Red,
+        ChinesePixelFont.Outline,
         1,
         1
     )
-})
-
-player.onChat("清除", function () {
-    chinesePixel.clearArea(pos(0, 1, 5), EAST, 100, 50)
 })
